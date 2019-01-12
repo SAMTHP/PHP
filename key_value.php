58206ed47bc463bc07477c20f414d59f6d19903f
SAMTHP <samir_615@live.fr>
2019-01-12 17:14:06 +0100

    maj

--- key_value.php
+++ key_value.php
@@ -1,11 +1,15 @@
 <?php
-	$tab = array("French"=>"Paris","Morocco"=>"Rabat","Iran"=>"Teheran","ArabiaMaudite"=>"Neom","USA"=>"Babylon");
-	print_r($tab);
+	$tab = array("French"=>"Paris","Morocco"=>"Rabat","Iran"=>"TeheRan","ArabiaMaudite"=>"Neom","USA"=>"Babylon");
+	// print_r($tab);
 	$i = 0;
-	while ( $i <= count($tab)) {
-		if(strpos($tab[$i], "r")){
-			echo $tab[$i];
+	while ( $i < count($tab)) {
+		$capitale = strtolower(array_values($tab)[$i]);
+		 echo strpos($capitale, "r");
+		if(strpos($capitale, "r"))
+		{
+			print_r (array_values($tab)[$i]);
 		}
 		$i++;
 	}
+	// print_r(array_values($tab)[0]);
 ?>
